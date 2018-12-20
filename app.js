8d4a36a23e25e13195e510cd387012553aca9d8f
Andita Kaesar <anditakaesar@gmail.com>
2018-12-20 21:46:39 +0700

    separate router

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 
 // mongoose
 require('./utils/dbconn');
-var CheckList = require('./models/list');
 
 // bodyParser
 var bodyParser = require('body-parser');
@@ -28,101 +27,6 @@ app.get('/',
 );
 
 // routes '/list'
-app.get('/list',
-    (req, res) => {        
-        CheckList.find({}).sort({ date: -1 }).exec((err, checklists) => {
-            return res.json({
-                success: true,
-                checklist: checklists
-            });
-        });
-    }
-);
-
-app.post('/list',
-    (req, res) => {
-        let id = req.body.id;
-        let desc = req.body.description;
-        let chkd = req.body.checked;
-        if (!id) return res.json({
-            success: false,
-            msg: 'id is null'
-        });
-        else if (!desc) return res.json({
-            success: false,
-            msg: 'description is null'
-        });
-        else if (chkd === undefined) return res.json({
-            success: false,
-            msg: 'checked is null'
-        });
-
-        CheckList.findOneAndUpdate(
-            { '_id': id }, 
-            { description: desc, checked: chkd }, 
-            { new: true }, 
-            (err, checklist) => {
-                if (err) {
-                    return res.json({ 
-                        success: false, 
-                        msg:'couldn\'t retrieve checklist due to error', 
-                        err: err 
-                    });
-                }
-
-                if (!checklist) return res.json({
-                    success: false,
-                    msg: 'checklist not found'
-                });
-                else return res.json({
-                    success: true,
-                    msg: 'checklist updated',
-                    checklist: checklist
-                });
-            });
-    }
-);
-
-app.get('/list/:id',
-    (req, res) => {
-        CheckList.findOne({ '_id': req.params.id }, (err, checklist) => {
-            if (err) return res.json({ 
-                success: false, 
-                msg:'couldn\'t retrieve checklist due to error', 
-                err: err 
-            });
-            
-            if (checklist && checklist.length == 0) return res.json({
-                success: false,
-                msg: 'checklist not found'
-            });
-            else return res.json({
-                success: true,
-                msg: 'check retrieved checklist',
-                checklist: checklist
-            });
-        });
-    }
-);
-
-app.put('/list',
-    (req, res) => {
-        var newList = new CheckList({ description: req.body.description });
-        newList.save().then(() => {
-            console.log('an item saved');
-            return res.json({
-                success: true,
-                list: newList
-            });
-        });
-    }
-);
-
-app.delete('/list',
-    (req, res) => res.send('Delete a list')
-);
-
-// end routes '/list' 
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/list', require('./routes/list'));
 
 module.exports = app;
---
+++ routes/list.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+var CheckList = require('../models/list');
+
+// a router for /list
+
+router.get('/',
+    (req, res) => {        
+        CheckList.find({}).sort({ date: -1 }).exec((err, checklists) => {
+            return res.json({
+                success: true,
+                checklist: checklists
+            });
+        });
+    }
+);
+
+router.post('/',
+    (req, res) => {
+        let id = req.body.id;
+        let desc = req.body.description;
+        let chkd = req.body.checked;
+        if (!id) return res.json({
+            success: false,
+            msg: 'id is null'
+        });
+        else if (!desc) return res.json({
+            success: false,
+            msg: 'description is null'
+        });
+        else if (chkd === undefined) return res.json({
+            success: false,
+            msg: 'checked is null'
+        });
+
+        CheckList.findOneAndUpdate(
+            { '_id': id }, 
+            { description: desc, checked: chkd }, 
+            { new: true }, 
+            (err, checklist) => {
+                if (err) {
+                    return res.json({ 
+                        success: false, 
+                        msg:'couldn\'t retrieve checklist due to error', 
+                        err: err 
+                    });
+                }
+
+                if (!checklist) return res.json({
+                    success: false,
+                    msg: 'checklist not found'
+                });
+                else return res.json({
+                    success: true,
+                    msg: 'checklist updated',
+                    checklist: checklist
+                });
+            });
+    }
+);
+
+router.get('/:id',
+    (req, res) => {
+        CheckList.findOne({ '_id': req.params.id }, (err, checklist) => {
+            if (err) return res.json({ 
+                success: false, 
+                msg:'couldn\'t retrieve checklist due to error', 
+                err: err 
+            });
+            
+            if (checklist && checklist.length == 0) return res.json({
+                success: false,
+                msg: 'checklist not found'
+            });
+            else return res.json({
+                success: true,
+                msg: 'check retrieved checklist',
+                checklist: checklist
+            });
+        });
+    }
+);
+
+router.put('/',
+    (req, res) => {
+        var newList = new CheckList({ description: req.body.description });
+        newList.save().then(() => {
+            console.log('an item saved');
+            return res.json({
+                success: true,
+                list: newList
+            });
+        });
+    }
+);
+
+router.delete('/',
+    (req, res) => res.send('Delete a list')
+);
+
+module.exports = router;
