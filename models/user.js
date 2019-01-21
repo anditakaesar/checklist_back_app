1f87ac7f00583c32816ff92e1669f7c8124919bc
anditakaesar <anditakaesar@gmail.com>
2019-01-20 21:24:36 -0800

    add user model

---
+++ models/user.js
@@ -0,0 +1,64 @@
+// a model of list
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcryptjs = require('bcryptjs');
+
+var userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordConf: {
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    bcryptjs.hash(user.password, 10, (err, hash) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            user.password = hash;
+        }
+        next();
+    });
+});
+
+userSchema.statics.authenticate = function (email, password, callback) {
+    User.findOne({ email: email })
+        .exec(function (err, user) {
+            if (err) {
+                return callback(err);
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcryptjs.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback();
+                }
+            });
+        })
+};
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = exports = User;
