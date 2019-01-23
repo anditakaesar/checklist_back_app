e2a03c8ccfdcdac44fe56763b4b905efeb4b9d7c
anditakaesar <anditakaesar@gmail.com>
2019-01-23 09:02:50 +0700

    add new param conn, useCreateIndex

--- utils/dbconn.js
+++ utils/dbconn.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 var mongodbconn = process.env.MONGODB_URI || 'mongodb://localhost/CheckListApp';
 
 const connection = mongoose.connect(mongodbconn, 
-    { useNewUrlParser: true },
+    { useNewUrlParser: true, useCreateIndex: true },
     err => {
         if (err) {
             console.log('couldn\'t connect to database');
