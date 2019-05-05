bc452b340d0c1c661e18fd3f7a2c35abcf5bd6ef
anditakaesar <anditakaesar@gmail.com>
2019-05-05 09:55:27 +0700

    remove user model default authentication

--- models/user.js
+++ models/user.js
@@ -39,26 +39,6 @@ userSchema.pre('save', function (next) {
     });
 });
 
-userSchema.statics.authenticate = function (email, password, callback) {
-    User.findOne({ email: email })
-        .exec(function (err, user) {
-            if (err) {
-                return callback(err);
-            } else if (!user) {
-                err = new Error('User not found.');
-                err.status = 401;
-                return callback(err);
-            }
-            bcryptjs.compare(password, user.password, function (err, result) {
-                if (result === true) {
-                    return callback(null, user);
-                } else {
-                    return callback();
-                }
-            });
-        });
-};
-
 var User = mongoose.model('User', userSchema);
 
 module.exports = exports = User;
