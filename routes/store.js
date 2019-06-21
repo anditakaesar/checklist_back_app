8dce011230b494abadfbcbf6d1a52f9e33907b0a
andita.kaesar <andita.kaesar@xtremax.com>
2019-06-21 14:08:01 +0700

    fix store route

--- routes/store.js
+++ routes/store.js
@@ -1,11 +1,14 @@
-// route for store management
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    return res.status(200).json({
-        message: 'all store will be showed here';
-    });
-});
+router.get('/',
+    (req, res) => {
+        return res.status(200).json({
+            success: true,
+            message: 'should be ok'
+        });
+    }
+);
+
 
 module.exports = router;
